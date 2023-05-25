7c67fbecd6378954c414ac07fa7c9b2e523ebb72
eyder-suarez <ey_der19@hotmail.com>
2023-05-24 23:41:44 -0500

    Logica del login y validaciones del formulario

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +11,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
 
-
+  public error!: string
   public myForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private userService: UsersService, private router: Router) {}
 
   ngOnInit(): void {
 
+    /**
+     * Formulario del login
+     */
     this.myForm = this.fb.group({
       user: ['', [Validators.required,]],
       password: ['', [Validators.required]]
@@ -24,9 +29,34 @@ export class LoginComponent implements OnInit {
   }
 
 
+  /**
+   * Metodo para realizar el login del usuario, si existe continua de lo contrario arrojara un error
+   */
   save() {
-    console.log(this.myForm.value);
+    let user     = this.myForm.get('user')?.value
+    let password = this.myForm.get('password')?.value
 
+    this.userService.auth(user, password).subscribe((getuser) => {
+      if(getuser[0]?.user === user && getuser[0]?.password === password) {
+        this.router.navigate(['/table'])
+      }else {
+        this.error = 'Usuario o contraseña incorrectos'
+        this.myForm.reset()
+      }
+
+    })
+
+  }
+
+
+  /**
+   *
+   * Para validar los campos del formulario
+   * @returns
+   */
+  invalidField( field: string ) {
+    return this.myForm.get(field)?.invalid
+            && this.myForm.get(field)?.touched;
   }
 
 
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 export class UsersService {
 
 
-  private url: string = "https://ivuzyxgtkatfcaybdlst.supabase.co/rest/v1/users"; //URL para traer todos los usuarios
+  private url: string = environment.supabaseurl; //URL para traer todos los usuarios
 
 
 
@@ -18,15 +18,27 @@ constructor(private http: HttpClient) { }
 
 /**
  *
- * @returns Trae la tabla de los clientes
+ * @returns Trae la tabla de los usuarios
  */
 getUser() {
   let headers = new HttpHeaders({
     'apikey'       : environment.supabaseKey,
     'Authorization': environment.authorization
   })
-   return this.http.get<any>(this.url, {headers}).pipe()
+   return this.http.get<any>(`${this.url}`, {headers}).pipe()
 }
 
+
+/**
+ *
+ * @returns si el usuario y el password existen realiza el login
+ */
+auth(user: string, password: string)  {
+  let headers = new HttpHeaders({
+    'apikey'       : environment.supabaseKey,
+    'Authorization': environment.authorization
+  })
+   return this.http.get<any>(`${this.url}?user=eq.${user}&password=eq.${password}`, {headers}).pipe()
+}
 
 }
